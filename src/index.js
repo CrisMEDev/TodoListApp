485a7dc0517b6e4afe1bfaf89d3d08d0980ca5c7
Cristian Morales <cr15moresp@gmail.com>
2021-04-17 14:44:55 -0500

    Se implementó la clase para manejar todas las tareas creadas; se declararon los metodos que se necesitaran

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import { Todo } from './classes/todo.class.js';
+import { Todo, TodoList } from './classes'         // Cuando no se específica un archivo, buscará el index.js por defecto
 
 import './styles.css';
 
+const todoList = new TodoList();
 
 const tarea = new Todo('Aprender JavaScript');
+const tarea2 = new Todo('Comprar un unicornio');
 
-console.log(tarea);
+todoList.nuevoTodo(tarea);
+todoList.nuevoTodo(tarea2);
+
+// console.log(tarea);
+console.log(todoList);
