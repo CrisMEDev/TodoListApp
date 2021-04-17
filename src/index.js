067578acb433df9173c5b7e70a44173b87a6f972
Cristian Morales <cr15moresp@gmail.com>
2021-04-17 15:48:29 -0500

    Se implementó el método para constriur tareas en html

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import { Todo, TodoList } from './classes'         // Cuando no se específica un archivo, buscará el index.js por defecto
+import { crearTodoHtml } from './js/componenetes';
 
 import './styles.css';
 
 const todoList = new TodoList();
 
 const tarea = new Todo('Aprender JavaScript');
-const tarea2 = new Todo('Comprar un unicornio');
 
 todoList.nuevoTodo(tarea);
-todoList.nuevoTodo(tarea2);
 
 // console.log(tarea);
 console.log(todoList);
+
+crearTodoHtml( tarea );
--- src/js/componenetes.js
+++ src/js/componenetes.js
@@ -1,12 +1,27 @@
-import '../css/componentes.css';
+
+// Referencias en el html
+const divTodoList = document.querySelector('.todo-list');
 
 
-export const f_saludar = (nombre) => {
-    console.log('Creando etiqueta h1');
+export const crearTodoHtml = ( todo ) => {
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${nombre} como estás, cuéntame?`;
-    document.body.append( h1 );
+    const htmlTodo = `
+    <li class="${ (todo.completado) ? 'completed' : '' }" data-id="${ todo.id }">
+        <div class="view">
+            <input class="toggle" type="checkbox" ${ (todo.completado) ? 'checked' : '' }>
+            <label>${ todo.tarea }</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    </li>
+    `;
+
+    const div = document.createElement('div');          // Se agrega dentro del div la etiqueta en la variable htmlTodo
+    div.innerHTML = htmlTodo;
+
+    divTodoList.append(div.firstElementChild);          // INSERTA EL PRIMER HIJO (PARA NO INSERTAR EL DIV Y SOLO EL li)
+
+    return div.firstElementChild;
+
 }
-
 
